f3e293c1a750bc9f422b72f6a6e6ec77b741b06b
Mac Slocum <macslocum@gmail.com>
2025-06-02 10:49:33 -0400

    Add files via upload

---
+++ original/mtview.php
@@ -0,0 +1,5 @@
+<?php
+    include('/home/dh_fngh2q/original.mikeswashingtonwatch.com/cgi-bin/movabletype/php/mt.php');
+    $mt = new MT(1, '/home/dh_fngh2q/original.mikeswashingtonwatch.com/cgi-bin/movabletype/mt-config.cgi');
+    $mt->view();
+?>
